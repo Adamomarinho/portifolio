11d709ae560c826cb43eb50e5c2bdab516f00576
Adamo <98761314+Adamomarinho@users.noreply.github.com>
2024-06-17 16:16:59 -0300

    Create retorno_cep.php

---
+++ cepapi/src/controller/retorno_cep.php
@@ -0,0 +1,33 @@
+<?php
+
+    include_once "../model/valida_cep.php";
+
+    function retornar_cep()
+    {
+        $cep = $_GET["cep"];
+        result($cep);
+        if(isset($GLOBALS["rua"]) && isset($GLOBALS["bairro"]) && isset($GLOBALS["comp"]) && isset($GLOBALS["cidade"]) && isset($GLOBALS["estado"]))
+        {
+            if(!empty($GLOBALS["comp"]))
+            {
+                $rua = $GLOBALS["rua"];
+                $bairro = $GLOBALS["bairro"];
+                $comp = $GLOBALS["comp"];
+                $cidade = $GLOBALS["cidade"];
+                $estado = $GLOBALS["estado"];
+    
+                echo "<p>Seu endereço é: $rua, $comp, $bairro, $cidade - $estado.</p>";
+            }
+            else
+            {
+                $rua = $GLOBALS["rua"];
+                $bairro = $GLOBALS["bairro"];
+                $cidade = $GLOBALS["cidade"];
+                $estado = $GLOBALS["estado"];
+    
+                echo "<p>Seu endereço é: $rua, $bairro, $cidade - $estado.</p>";
+            }
+        }
+    }
+
+?>
