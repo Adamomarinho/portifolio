75faae7873495f430cfbf75dea67723b7e481856
Adamo <98761314+Adamomarinho@users.noreply.github.com>
2024-06-17 16:12:13 -0300

    Update conexao.php

--- cepapi/src/model/conexao.php
+++ cepapi/src/model/conexao.php
@@ -1,1 +1,20 @@
+<?php
+    function conexao($c){
+        $cep = $c;
+        $ch = curl_init();
+        $url = 'viacep.com.br/ws/'.$cep.'/json/';
+        curl_setopt_array($ch, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+        ]);
 
+        global $return;
+
+        $con = curl_exec($ch);
+        $return = json_decode($con, true);
+
+        curl_close($ch);
+    }
+    
+
+?>
