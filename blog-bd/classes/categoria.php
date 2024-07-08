d73a61d20b65f58cec2536038ff1ce94d234bc41
Adamo <98761314+Adamomarinho@users.noreply.github.com>
2024-07-08 17:09:47 -0300

    Add files via upload

--- blog-bd/classes/categoria.php
+++ blog-bd/classes/categoria.php
@@ -2,15 +2,15 @@
 
 require_once 'classe.php';
 
-class NivelCrud
-{	
-	function SalvarNivel($nome)
+class CategoriaCrud
+{
+	function SalvarCategoria($nome)
 	{
 		try
 		{
             $conecta = new Sistema();
             $link = $conecta->conectado();
-			$sql = $link->prepare("INSERT INTO nivel (nomenivel) VALUES (:nome)");
+			$sql = $link->prepare("INSERT INTO categoria (nomecat) VALUES (:nome)");
 			$sql->bindparam(":nome",$nome);
 			$sql->execute();
             return true;
@@ -20,69 +20,69 @@ class NivelCrud
 			echo $e->getMessage();
             return false;
 		}
-		
+
 	}
 
-    function PegaDadosNivel($id, $campo)
+    function PegaDadosCategoria($id, $campo)
 	{
         $conecta = new Sistema();
         $link = $conecta->conectado();
-        
-        $sql = $link->prepare("SELECT * FROM nivel WHERE idnivel=:id");
+
+        $sql = $link->prepare("SELECT * FROM categoria WHERE idcat=:id");
 		$sql->execute([':id' => $id]);
 		$dados=$sql->fetch(PDO::FETCH_ASSOC);
 		return $dados[$campo];
 	}
-	
-	function AlteraNivel($id, $nome)
+
+	function AlteraCategoria($id, $nome)
 	{
 		try
 		{
             $conecta = new Sistema();
             $link = $conecta->conectado();
-			$sql = $link->prepare("UPDATE nivel SET nomenivel=:nome WHERE idnivel=:id");
+			$sql = $link->prepare("UPDATE categoria SET nomecat=:nome WHERE idcat=:id");
 			$sql->bindparam(":nome",$nome);
 			$sql->bindparam(":id",$id);
 			$sql->execute();
             if($sql->rowCount() > 0)
             {
-              msg('success','Nivel alterado com sucesso!');
+              msg('success','Categoria alterada com sucesso!');
               redireciona(3, 'gerenciar.php');
             }
             else
             {
-              msg('danger','Falha ao atualizar Nivel!');
+              msg('danger','Falha ao atualizar Categoria!');
               redireciona(3, 'gerenciar.php');
             }
             //return true;
 		}
 		catch(PDOException $e)
 		{
-            msg('danger','Falha ao atualizar Nivel!'.$e->getMessage());
+            msg('danger','Falha ao atualizar Categoria!'.$e->getMessage());
             redireciona(3, 'gerenciar.php');
-            //echo $e->getMessage();			
+            //echo $e->getMessage();
             //return false;
 		}
 	}
-	
-	function RemoverNivel($id, $destino)
+
+	function RemoverCategoria($id, $destino)
 	{
         $conecta = new Sistema();
         $link = $conecta->conectado();
-        $sql = $link->prepare("DELETE FROM nivel WHERE idnivel=:id");
+        $sql = $link->prepare("DELETE FROM categoria WHERE idcat=:id");
 		$sql->bindparam(":id",$id);
 		$sql->execute();
         if ($sql->rowCount())
         {
-            msg('success', 'Nivel excluido com sucesso!');
+            msg('success', 'Categoria excluido com sucesso!');
             redireciona(3, $destino);
         }
         else
         {
-            msg('danger', 'Não foi possivel remover o Nivel');
+            msg('danger', 'Não foi possivel remover o Categoria');
             redireciona(3, $destino);
         }
 		//return true;
 	}
-	
+
 }
