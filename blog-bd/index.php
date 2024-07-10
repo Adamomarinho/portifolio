3abc42697873530349934bb6d1f49d09660847bc
Adamo <98761314+Adamomarinho@users.noreply.github.com>
2024-07-10 17:55:12 -0300

    Add files via upload

---
+++ blog-bd/index.php
@@ -0,0 +1,8 @@
+<?php    
+
+require 'classes/classe.php';
+
+$destino = 'blog.php';
+redireciona(1, $destino);
+
+?>
---
+++ blog-bd/registra.php
@@ -0,0 +1,49 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Blog</title>
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+
+  </head>
+<body class="text-center">
+    <br><br><br><br><br>
+<?php
+
+require 'classes/usuario.php';
+
+$user = new UsuarioCrud();
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$pega_senha = $_POST['senha'];
+$senha = password_hash($pega_senha, PASSWORD_DEFAULT);
+$nivel = 3;
+$situacao = 3;
+$destino = 'admin/login.php';
+
+if($user->SalvarUsuarioExterno($nome,$email,$senha,$situacao,$nivel))
+{
+  msg('success', 'Usuario adicionado com sucesso!');
+  redireciona(3, $destino);
+}
+else
+{
+  msg('danger', 'Não foi possivel adicionar o Usuario');
+  redireciona(3, $destino);
+}
+
+?>
+<script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
