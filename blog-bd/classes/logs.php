9c8db75c31b1ebce9a8321ab4dfef258c2642f80
Adamo <98761314+Adamomarinho@users.noreply.github.com>
2024-07-12 13:14:47 -0300

    Add files via upload

--- blog-bd/classes/logs.php
+++ blog-bd/classes/logs.php
@@ -4,17 +4,16 @@ require_once 'classe.php';
 
 class Logs
 {
-	function SalvarLogs($usuario, $ip, $acao, $data_acao )
+	function SalvarLogs($usuario, $ip, $acao )
 	{
 		try
 		{
             $conecta = new Sistema();
             $link = $conecta->conectado();
-			$sql = $link->prepare("INSERT INTO permissao (usuario, ip, acao, data_acao) VALUES (:usuario, :ip, :acao, :data_acao)");
+			$sql = $link->prepare("INSERT INTO logs (usuario, ip, acao, data_acao) VALUES (:usuario, :ip, :acao, NOW())");
 			$sql->bindparam(":usuario",$usuario);
             $sql->bindparam(":ip",$ip);
             $sql->bindparam(":acao",$acao);
-            $sql->bindparam(":data_acao",$data_acao);
 			$sql->execute();
             return true;
 		}
@@ -31,10 +30,29 @@ class Logs
         $conecta = new Sistema();
         $link = $conecta->conectado();
 
-        $sql = $link->prepare("SELECT * FROM Permissao WHERE id=:id");
+        $sql = $link->prepare("SELECT * FROM logs WHERE id=:id");
 		$sql->execute([':id' => $id]);
 		$dados=$sql->fetch(PDO::FETCH_ASSOC);
 		return $dados[$campo];
+	}
+
+	function PegaIp() {
+		$ipaddress = '';
+		if (isset($_SERVER['HTTP_CLIENT_IP']))
+			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+		else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		else if(isset($_SERVER['HTTP_X_FORWARDED']))
+			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+		else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+		else if(isset($_SERVER['HTTP_FORWARDED']))
+			$ipaddress = $_SERVER['HTTP_FORWARDED'];
+		else if(isset($_SERVER['REMOTE_ADDR']))
+			$ipaddress = $_SERVER['REMOTE_ADDR'];
+		else
+			$ipaddress = 'UNKNOWN';
+		return $ipaddress;
 	}
 
 }
